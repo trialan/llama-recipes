a969f77ce06689e51e3fa3160305a93d7bd2daf2
Thomas Rialan <thomasrialan@gmail.com>
2023-10-03 18:41:52 +0100

    test ds

--- src/llama_recipes/datasets/test.py
+++ src/llama_recipes/datasets/test.py
@@ -1,7 +1,10 @@
 from llama_recipes.datasets import get_biblechat_dataset, get_alpaca_dataset
+from llama_recipes.configs.datasets import alpaca_dataset, biblechat_dataset
+from gloohack.modelling.config import get_tokenizer
 
 if __name__ == '__main__':
-    bc = get_biblechat_dataset()
-    ap = get_alpaca_dataset()
+    tokenizer = get_tokenizer()
+    bc = get_biblechat_dataset(biblechat_dataset(), tokenizer, "test")
+    ap = get_alpaca_dataset(alpaca_dataset(), tokenizer, "val")
 
 
