4e4343600093a2c8ac6a070368a7a74cbcc8de2b
Thomas Rialan <thomasrialan@gmail.com>
2023-10-03 18:46:58 +0100

    edit tests

--- src/llama_recipes/datasets/test.py
+++ src/llama_recipes/datasets/test.py
@@ -1,11 +1,11 @@
 from llama_recipes.datasets import get_biblechat_dataset, get_samsum_dataset
-from llama_recipes.configs.datasets import samsun_dataset, biblechat_dataset
+from llama_recipes.configs.datasets import samsum_dataset, biblechat_dataset
 from gloohack.modelling.config import get_tokenizer
 
 
 if __name__ == '__main__':
     tokenizer = get_tokenizer()
     bc = get_biblechat_dataset(biblechat_dataset(), tokenizer, "test")
-    ap = get_samsun_dataset(samsun_dataset(), tokenizer, "validation")
+    ap = get_samsum_dataset(samsun_dataset(), tokenizer, "validation")
 
 
