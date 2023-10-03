b87e26488f1fff64a4f189784ee004ffd111ce54
Thomas Rialan <thomasrialan@gmail.com>
2023-10-03 15:21:01 +0100

    torch dataset not hf

--- src/llama_recipes/datasets/biblechat_dataset.py
+++ src/llama_recipes/datasets/biblechat_dataset.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+from torch.utils.data import Dataset
 from llama_recipes.configs.datasets import biblechat_dataset
 
 from gloohack.dataset.script import get_dataset
@@ -10,9 +11,24 @@ DATASET = "tr416/gloo_dataset_v2"
 TOKENIZED_DATASET = "tr416/gloo_dataset_v2_tokenized"
 
 
+def huggingface_to_pytorch(hf_dataset):
+    class CustomPyTorchDataset(Dataset):
+        def __init__(self, hf_dataset):
+            self.hf_dataset = hf_dataset
+
+        def __len__(self):
+            return len(self.hf_dataset)
+
+        def __getitem__(self, idx):
+            return self.hf_dataset[idx]
+    return CustomPyTorchDataset(hf_dataset)
+
+
 def get_custom_dataset(dataset_config, tokenizer, split):
-    ds = load_dataset(TOKENIZED_DATASET, split=split, use_auth_token=True)
-    return ds
+    hf_ds = load_dataset(TOKENIZED_DATASET, split=split, use_auth_token=True)
+    torch_ds = huggingface_to_pytorch(hf_ds)
+    return torch_ds
+
 
 def load_custom_dataset(dataset_config, tokenizer, split):
     dataset = load_dataset(DATASET, split=split, use_auth_token=True)
@@ -38,3 +54,8 @@ def format_as_str(dialog_pair):
 if __name__ == '__main__':
     tokenizer = get_tokenizer()
     ds = get_custom_dataset(biblechat_dataset(), tokenizer, "train")
+
+    dataset = load_dataset(DATASET, use_auth_token=True)
+    fmt_dataset = dataset.map(lambda x: format_dialog(x, tokenizer))
+    fmt_dataset = fmt_dataset.remove_columns(['user', 'llm'])
+    fmt_dataset = fmt_dataset.train_test_split(0.15)
