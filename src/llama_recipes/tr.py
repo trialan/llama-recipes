ce11b535c967b0653387ac5f0df6a583650edb23
Thomas Rialan <thomasrialan@gmail.com>
2023-10-04 16:06:38 +0100

    eval tr.py

--- src/llama_recipes/tr.py
+++ src/llama_recipes/tr.py
@@ -17,24 +17,35 @@ from transformers import AutoModelForCausalLM, LlamaTokenizer, LlamaForCausalLM
 from gloohack.modelling.llama import get_completion
 
 
+def load_ckpt(base, path):
+    """ Offload to avoid RAM OOM, merge_and_unload for normal model fmt """
+    peft_model = PeftModel.from_pretrained(base,
+                                           path,
+                                           offload_folder="tmp")
+    model = peft_model.to(0)
+    model = model.merge_and_unload()
+    return model
+
+
 
 if __name__ == '__main__':
     print("Loading model")
     base_model = LlamaForCausalLM.from_pretrained(kwargs['model_name'])
 
-    print("Loading PEFT model")
-    peft_path = "/home/paperspace/llama-recipes/src/llama_recipes/tr_models/checkpoint_6000/"
-    peft_model = PeftModel.from_pretrained(base_model, peft_path, offload_folder="tmp") #offload to not run out of RAM
-    model = peft_model.to(0)
-    model = model.merge_and_unload() #converts back to not-peft-model
-
     print("Loading tokenizer")
     tokenizer = LlamaTokenizer.from_pretrained(kwargs['model_name'])
     tokenizer.pad_token = tokenizer.eos_token
 
+    print("Loading PEFT model")
+    peft_paths = glob.glob("/home/paperspace/tr_models/*checkpoint*")
+    paths = sorted(peft_paths)
+
     print("Testing")
-    s = "Does Jesus like me?"
-    x = get_completion(s, model, tokenizer)
-    print(x)
+    for p in paths:
+        print(f"CKPT: {p}")
+        model = load_ckpt(base_model, p)
+        s = "Does Jesus like me?"
+        x = get_completion(s, model, tokenizer)
+        print(x)
 
 
